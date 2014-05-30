47bbf9b1415864c3a299aa3e05f280f16f64f8bf
Ldouchet <douchet.loic55@gmail.com>
2014-05-30 23:08:57 +0200

    Update Unit_test_Competition.php

--- tests/Unit_test_Competition.php
+++ tests/Unit_test_Competition.php
@@ -8,15 +8,10 @@
  *
  *
  * Ceci est un exemple de test unitaire qui vérifie la création d'une compétition.
- * "PHPUnit_Framework_TestCase" provient du framework de tests unitaires PHPUnit open source dédié au langage de programmation PHP
- */
-require_once('Competition.php');
+ 
+require_once('../classes/Competition.php');
 
-class Unit_test_Competition extends PHPUnit_Framework_TestCase {
-
-    function Unit_test_Competition() {
-        $this->Unit_test_Competition();
-    }
+class Unit_test_Competition  {
 
     Function testCreation() {
         $comp = new Competition();
